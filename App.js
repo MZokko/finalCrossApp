6a9df87f305166bdee18a0e4d703c3d194239ef8
alexandre <akekiwy@gmail.com>
2020-01-26 15:48:27 +1100

    loadingscreen for the login modul

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 //react nav 
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-
 //screen
 import Loginscreen from './screens/Loginscreen';
 import Mainscreen from './screens/Mainscreen';
+import LoadingScreen from './screens/LoadingScreen';
+//firebase
+import firebase from 'firebase';
+import firebaseConfig from './config';
+firebase.initializeApp(firebaseConfig);
 
 const MainNavigator = createSwitchNavigator({
+  Loading: { screen: LoadingScreen },
   Login: { screen: Loginscreen },
   Main: { screen: Mainscreen },
 });
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDw2fyDuV4Z-XW4tKPn4F-UvarQcskt-yw",
+    authDomain: "crossplatfin.firebaseapp.com",
+    databaseURL: "https://crossplatfin.firebaseio.com",
+    projectId: "crossplatfin",
+    storageBucket: "crossplatfin.appspot.com",
+    messagingSenderId: "1083599611362",
+    appId: "1:1083599611362:web:dca1ea142ee9062bb505bb"
+  };
