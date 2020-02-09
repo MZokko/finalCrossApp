e2058966271d9031cefc995abc89428a7649efc1
alexandre <akekiwy@gmail.com>
2020-02-10 09:22:35 +1100

    hall of fame fix

--- screens/HallOfFameScreen.js
+++ screens/HallOfFameScreen.js
@@ -1,34 +1,67 @@
 import React, { Component } from "react";
-import {
-  View,
-  StyleSheet,
-  Button
-} from "react-native";
+import { View, StyleSheet, Button, Text } from "react-native";
 import firebase from "firebase";
 import Card from "../Game/components/Card";
 import Header from "../Game/components/Header";
 import color from "../Game/constants/color";
-
-
+import { ScrollView } from "react-native-gesture-handler";
 
 export default class HallOfFameScreen extends Component {
-      setupHighscoreListener=()=>{
-        firebase.database().ref('HallOfFame').on('value',(snapshot) => {
-            console.log("what u get",snapshot);
-          snapshot.forEach(Object => {
-              console.log("just the object ?",snapshot.val())
-          });
-        });}
+  state = {
+    hallOfFameScore: []
+  };
+  setupHighscoreListener = () => {
+    firebase
+      .database()
+      .ref("HallOfFame")
+      .on("value", snapshot => {
+        const fame = snapshot.val();
+        const item = Object.values(fame);
+        this.setState({ hallOfFameScore: item });
+      });
+    //console.log("item====",item);
+    console.log("state=====", this.state.hallOfFameScore);
+    console.log(
+      "my object transc==",
+      Object.keys(this.state.hallOfFameScore).map(key => ({
+        roundsNumber: key.valueOf()
+      }))
+    );
+  };
 
   render() {
-    
     return (
       <View>
         <Header title="Hall of Fame" />
-        <Card>
-          <View >
-              <Button title="try" onPress={()=>{this.setupHighscoreListener()}}/>
-            {/* <FlatList
+
+        <View>
+          <Button
+            title="LOAD"
+            color={color.accent}
+            onPress={() => {
+              this.setupHighscoreListener();
+            }}
+          />
+          <View>
+            <ScrollView>
+              {this.state.hallOfFameScore.map(hallOfFameScore => (
+                <Card>
+                  <Text key={hallOfFameScore.roundsNumber}>
+                    {hallOfFameScore.roundsNumber}
+                  </Text>
+                  <Text key={hallOfFameScore.userNumber}>
+                    {hallOfFameScore.userNumber}
+                  </Text>
+                </Card>
+              ))}
+            </ScrollView>
+          </View>
+          {/* {async()=>{
+              this.state.hallOfFameScore.forEach(Object.values(this.state.hallOfFameScore) => {
+                
+              });
+            }} */}
+          {/* <FlatList
               keyExtractor={item => JSON.stringify(item.firebaseId)}
               data={toDoList}
               renderItem={(taskData, idx) => (
@@ -39,8 +72,7 @@ export default class HallOfFameScreen extends Component {
                 />
               )}
             /> */}
-          </View>
-        </Card>
+        </View>
 
         <Button
           title="Menu"
@@ -50,18 +82,16 @@ export default class HallOfFameScreen extends Component {
           }}
         />
       </View>
-
-    );  
+    );
   }
 }
 const styles = StyleSheet.create({
-    screen: {
-      flex:1,
-    },
-    container:{
-      flex:2,
-      alignItems:"center",
-      justifyContent:"center",
-    }
-    
-  });
+  screen: {
+    flex: 1
+  },
+  container: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
